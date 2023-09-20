8a53ea35eebc092976778e0cec5faca8996c8df5
no7yao <144131719+no7yao@users.noreply.github.com>
2023-09-20 19:01:33 +0100

    Add files via upload

---
+++ alg/19.09(blok1).cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Class1
+{
+    static void Main()
+    {
+        int a, b, c = 0, d = 0, e = 0;
+        a = Convert.ToInt32(Console.ReadLine());
+        if (a > 0) e = a;
+        if ((a > 0) && (a % 3 == 0)) d = d + a;
+        if (a < 0) c = c + 1;
+        for (int i = 1; i < 10; i++)
+        {
+            b = Convert.ToInt32(Console.ReadLine());
+            if ((b > 0) && (e < 0)) e = b;
+            if (a < b) a = b;
+            if (b < 0) c = c + 1;
+            if ((b > 0) && (b % 3 == 0)) d = d + b;
+            if ((b < e) && (b > 0)) e = b;
+        }
+        Console.WriteLine(a);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(e);
+
+    }
+
+}
