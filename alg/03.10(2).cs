0493c6b8c73fe788de55b6e672f763241f3e2e8f
no7yao <144131719+no7yao@users.noreply.github.com>
2023-10-03 12:38:59 +0600

    Add files via upload

---
+++ alg/03.10(2).cs
@@ -0,0 +1,23 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    int n, c, tri, pat, sem, cnt1;
+    cnt1 = 0;
+    n = Convert.ToInt32(Console.ReadLine());
+    for (int i=0; i<n; i++)
+    {
+    int a = Convert.ToInt32(Console.ReadLine());
+    c = a;
+    while (c>0){
+        if (c%10 == 7) 
+        {
+            cnt1++;
+            break;
+        }
+    
+        c /=10;
+    }
+    }
+    Console.WriteLine(cnt1);
+  }
+}
