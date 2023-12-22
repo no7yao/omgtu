542b2fa5833cab59322859300ba68a5b396c0d37
no7yao <144131719+no7yao@users.noreply.github.com>
2023-12-22 15:48:59 +0300

    Update 14.12pes.cs

--- alg/14.12pes.cs
+++ alg/14.12pes.cs
@@ -1,53 +1,69 @@
 using System;
 using System.Collections.Generic;
-class Dog
+class dog
 {
-    public int year { get; set; }
-    public string poroda { get; set; }
-    public string color { get; set; }
-    public string pol { get; set; }
-    public string name { get; set; }
+    private int year;
+    private string poroda;
+    private string color;
+    private string pol;
+    private string name;
+    public int Year { get => year; set => year = value; }
+    public string Poroda { get => poroda; set => poroda = value; }
+    public string Color { get => color; set => color = value; }
+    public string Pol { get => pol; set => pol = value; }
+    public string Name { get => name; set => name = value; }
 }
-class Program
+class program
 {
     static void Main()
     {
-        List<Dog> dogs = new List<Dog>();
-
+        List<dog> dogs = vvod();
+        smenacolor(dogs);
+        vivod(dogs);
+    }
+    public static List<dog> vvod()
+    {
+        List<dog> dogs = new List<dog>();
         Console.Write("Введите количество собак: ");
-        int count = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < count; i++)
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
         {
-            Dog dog = new Dog();
-            Console.Write("Год рождения: ");
-            dog.year = int.Parse(Console.ReadLine());
-            Console.Write("Порода: ");
-            dog.poroda = Console.ReadLine();
-            Console.Write("Окрас: ");
-            dog.color = Console.ReadLine();
-            Console.Write("Пол: ");
-            dog.pol = Console.ReadLine();
-            Console.Write("Кличка: ");
-            dog.name = Console.ReadLine();
+            dog dog = new dog();
+            Console.Write("Год рождения собаки: ");
+            dog.Year = int.Parse(Console.ReadLine());
+            Console.Write("Порода собаки: ");
+            dog.Poroda = Console.ReadLine();
+            Console.Write("Окраска собаки: ");
+            dog.Color = Console.ReadLine();
+            Console.Write("Пол собаки: ");
+            dog.Pol = Console.ReadLine();
+            Console.Write("Имя собаки: ");
+            dog.Name = Console.ReadLine();
             dogs.Add(dog);
         }
+        return dogs;
+    }
+    public static void smenacolor(List<dog> dogs)
+    {
         Console.Write("Введите породу для выборки: ");
-        string vibrporoda = Console.ReadLine();
+        string poroda1 = Console.ReadLine();
         Console.Write("Введите год рождения для выборки: ");
-        int vibryear = int.Parse(Console.ReadLine());
-        Console.Write("Введите новый окрас: ");
+        int year1 = int.Parse(Console.ReadLine());
+        Console.Write("Введите новый окрас собак: ");
         string newcolor = Console.ReadLine();
         foreach (var dog in dogs)
         {
-            if (dog.poroda == vibrporoda && dog.year == vibryear)
+            if (dog.Poroda == poroda1 && dog.Year == year1)
             {
-                dog.color = newcolor;
+                dog.Color = newcolor;
             }
         }
+    }
+    public static void vivod(List<dog> dogs)
+    {
         foreach (var dog in dogs)
         {
-            if (dog.poroda == vibrporoda && dog.year == vibryear) Console.WriteLine($"{dog.name} сменил окрас на {dog.color}");
+            Console.WriteLine($"{dog.Name} породы {dog.Poroda} {dog.Year} года рождения сменил цвет на {dog.Color}");
         }
     }
 }
